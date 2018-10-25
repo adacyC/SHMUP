2910a8784c67f7ccced80703ff229f7ee85dccc1
Flynn Ringrose <flynnringrose@gmail.com>
2018-10-24 21:11:02 -0300

    Initial setup

---
+++ Source/SHMUP/SHMUP.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SHMUP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SHMUP, "SHMUP" );
---
+++ Source/SHMUP/SHMUPGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SHMUPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHMUP_API ASHMUPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
